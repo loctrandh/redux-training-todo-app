72a0b6c2cf383646e2ff40ddc77a1d34e154140d
Loc Tran <loc_tran@datahouse.com>
2022-06-11 10:45:57 +0700

    👌add redux-logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
 import rootReducer from 'reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
